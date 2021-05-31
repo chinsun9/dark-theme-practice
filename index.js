7dec0db63bdfb4edc1914e567410da2fb6e915e1
chinsun9 <chin_sung@naver.com>
2021-05-31 15:29:32 +0900

    feat save, load theme

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function initTheme() {
-    const isDark = window.matchMedia("(prefers-color-scheme: dark)");
-    if (isDark) document.body.classList.add("dark");
+  // chk local storage
+  const theme = localStorage.getItem("theme");
+  if (theme === "dark") {
+    console.log(`load theme`);
+    document.body.classList.add("dark");
+    return;
   }
+  if (theme === "light") {
+    console.log(`load theme`);
+    return;
+  }
+
+  //   visit first time
+  const isDark = window.matchMedia("(prefers-color-scheme: dark)");
+  if (isDark) document.body.classList.add("dark");
+
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+}
 
 function toggleTheme() {
   console.log(`toggle theme`);
   document.body.classList.toggle("dark");
+  localStorage.setItem("theme", document.body.className ? "dark" : "light");
 }
 
 window
   .matchMedia("(prefers-color-scheme: dark)")
   .addEventListener("change", (e) => {
     const newColorScheme = e.matches ? "dark" : "light";
+    localStorage.setItem("theme", newColorScheme);
 
     if (newColorScheme === "dark") {
       document.body.classList.add("dark");
